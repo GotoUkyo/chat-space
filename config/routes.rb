a6867a8d9c733a218843093c58397863775dcf4a
GotoUkyo <itabiyo@yahoo.co.jp>
2020-12-04 14:49:19 +0900

    [編集]Groupsコントローラー
    チェックボックスを含むグループ新規作成データがモデルに保存されるよう編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :users, only: [:edit, :update]
-  get "groups/group"
   resources :groups, only: [:new, :create]
 end
