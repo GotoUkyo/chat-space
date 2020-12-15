4d96070f541bfa1c8ffd06746855e56bf1537ba5
GotoUkyo <itabiyo@yahoo.co.jp>
2020-12-15 15:58:12 +0900

    [追記]javascripts/users.js
    ・引数に値が入っていた場合
    ・引数に値が入っていなかった場合
    それぞれの場合にビューに追加するためのHTML（2種類）を作成

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,22 @@
 $(function() {
+  function addUser(user) {
+    let html = `
+                <div class="ChatMember clearfix">
+                  <p class="ChatMember__name">ユーザーの名前</p>
+                  <div class="ChatMember__add ChatMember__button" data-user-id="ユーザーのID" data-user-name="ユーザーの名前">追加</div>
+                </div>
+                 `;
+    $("#UserSearchResult").append(html);
+  }
+
+  function addNoUser() {
+    let html = `
+                <div class="ChatMember clearfix">
+                  <p class="ChatMember__name">ユーザーが見つかりません</p>
+                </div>
+                `;
+  }
+  
   $("UserSearch__field").on("keyup",function() {
     let input = $("UserSearch__field").val();
     $.ajax({
