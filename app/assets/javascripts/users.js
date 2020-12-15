531bedcd1fff5c065e4fbf159d03a2e8767842af
GotoUkyo <itabiyo@yahoo.co.jp>
2020-12-15 23:39:18 +0900

    [追記]groups/_form.html.haml
    ・編集画面にアクセスした時、既にグループに追加されているユーザーは最初から追加済みの状態であるように設定
    
    ・app/assets/javascripts/users.jsに記入漏れを発見したため修正

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -54,6 +54,12 @@ $(function() {
     });
   });
   $("#UserSearchResult").on("click", ".ChatMember__add", function() {
-    console.log
-  })
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this).parent().remove();
+    addMember(userName, userId);
+  });
+  $(".ChatMembers").on("click", ".ChatMember__remove", function() {
+    $(this).parent().remove();
+  });
 });
